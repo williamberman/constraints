35ab768d7953c1f5d4e57a334f61a9aa4ded5bb5
William Berman <wlbberman@gmail.com>
2020-01-20 00:58:41 -0500

    Fixed temperature tests

--- src/temperature.test.ts
+++ src/temperature.test.ts
@@ -1,3 +1,5 @@
+import { List } from 'immutable'
+
 import { fromPartial } from './network'
 import { PersistentNetwork } from './persistent-network'
 import { stdLib } from './std-lib'
@@ -21,11 +23,11 @@ describe('Temperature', () => {
 
     test('centigrade->farenheit', () => {
         net.setEqual(centigrade, net.constant(-40))
-        expect(net.valueOf(farenheit)).toEqual(-40)
+        expect(net.valueOf(farenheit)).toEqual(List([{ data: -40, type: 'bound' }]))
     })
 
     test('farenheit->centigrade', () => {
         net.setEqual(farenheit, net.constant(50))
-        expect(net.valueOf(centigrade)).toEqual(10)
+        expect(net.valueOf(centigrade)).toEqual(List([{ data: 10, type: 'bound' }]))
     })
 })
